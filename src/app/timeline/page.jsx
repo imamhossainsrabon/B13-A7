b36ad7fc65c3e4eab78413f6a92bab2c1df4492d
imamhossainsrabon <imamhossainsrabon@gmail.com>
2026-04-17 16:39:03 +0600

    inital setup done

---
+++ src/app/timeline/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Timeline = () => {
+    return (
+        <div>
+            <h2 className='text-6xl text-center font-bold'>TimeLIne components will be shown here</h2>
+        </div>
+    );
+};
+
+export default Timeline;
---
+++ src/test/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Test = () => {
+    return (
+        <div>
+            <h2 className='text-6xl text-center font-bold'>Test</h2>
+        </div>
+    );
+};
+
+export default Test;
