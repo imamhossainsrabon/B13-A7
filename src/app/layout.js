e1ec363dd448bb548b3d04057d6489ebc1a6763a
imamhossainsrabon <imamhossainsrabon@gmail.com>
2026-04-17 16:49:32 +0600

    test

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Nabvar from "@/components/homepage/Nabvar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -20,15 +19,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html
-      lang="en"      
-      data-theme="light"
+      lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">
-        <Nabvar />
-        {children}
-
-      </body>
+      <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
 }
